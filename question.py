52fb7db061292d40b3c3dd27a8551d1bc8f72d0e
cemhafiz <cem.hafizogullari@gmail.com>
2023-08-03 09:51:02 +0300

    second commit

--- question.py
+++ question.py
@@ -1,44 +1,54 @@
-usage: git [-v | --version] [-h | --help] [-C <path>] [-c <name>=<value>]
-           [--exec-path[=<path>]] [--html-path] [--man-path] [--info-path]
-           [-p | --paginate | -P | --no-pager] [--no-replace-objects] [--bare]
-           [--git-dir=<path>] [--work-tree=<path>] [--namespace=<name>]
-           [--config-env=<name>=<envvar>] <command> [<args>]
+#Taking input from the user
+val = input("Enter value: ")
 
-These are common Git commands used in various situations:
+#If length of the value is greater than 1 then process continues
+if len(val) > 1:
+   #Creating necessary variables, "temp" for checking substrings in a temporary string and "longest_substring" for keeping a list of longest substrings
+   temp = ""
+   longest_substring = []
 
-start a working area (see also: git help tutorial)
-   clone     Clone a repository into a new directory
-   init      Create an empty Git repository or reinitialize an existing one
+   #Scanning the value
+   for i in range(len(val) - 1):
+      #We need to scan the value starting from every letter to the end of the value to not miss any substrings, to do that, we need nested two loops
+      temp = val[i]
+      #Scanning the rest of the value starting from the current letter
+      for j in range(i, len(val) - 1):
+         #If we came to an end for the current substring because our temporary substring contains the next letter of the value on the scan
+         if val[j + 1] in temp:
+            #If the list doesn't contain any substrings we add temporary substring into it
+            if len(longest_substring) == 0:
+               longest_substring.append(temp)
+            #If the length of the substring we keep in the list is smaller than the length of the temporary substring, the list now only contains the temporary substring
+            elif len(longest_substring[0]) < len(temp):
+               longest_substring = [temp]
+            #If the length of the substring we keep in the list is equal to the length of the temporary substring and we aren't keeping the same substring already, we add the temporary substring to the list
+            elif len(longest_substring[0]) == len(temp) and temp not in longest_substring:
+               longest_substring.append(temp)
+            #After checking and editing of the list is done, temporary substring is set to the next letter of the value on the scan by breaking the inner loop
+            break
+            #PS: We only check if we came to an end for a better time complexity
+         #If temporary substring doesn't contain the next letter of the value on the scan, we add it to the temporary substring
+         else:
+            temp += val[j + 1]
+         #We need to check again before exiting the inner loop to not miss a substring
+         if j == len(val) - 2:
+            if len(longest_substring) == 0:
+               longest_substring.append(temp)
+            elif len(longest_substring[0]) < len(temp):
+               longest_substring = [temp]
+            elif len(longest_substring[0]) == len(temp) and temp not in longest_substring:
+               longest_substring.append(temp)
+   
+   #Printing the results
+   for x in longest_substring:
+      print(x + ", length: ", len(x))
 
-work on the current change (see also: git help everyday)
-   add       Add file contents to the index
-   mv        Move or rename a file, a directory, or a symlink
-   restore   Restore working tree files
-   rm        Remove files from the working tree and from the index
+#If length of the value is not greater than 1 then process comes to an end
+else:
+   if len(val) == 0:
+      print("You didn't enter any value!")
+   else:
+      print("Length of your value must be greater than 1!")
 
-examine the history and state (see also: git help revisions)
-   bisect    Use binary search to find the commit that introduced a bug
-   diff      Show changes between commits, commit and working tree, etc
-   grep      Print lines matching a pattern
-   log       Show commit logs
-   show      Show various types of objects
-   status    Show the working tree status
-
-grow, mark and tweak your common history
-   branch    List, create, or delete branches
-   commit    Record changes to the repository
-   merge     Join two or more development histories together
-   rebase    Reapply commits on top of another base tip
-   reset     Reset current HEAD to the specified state
-   switch    Switch branches
-   tag       Create, list, delete or verify a tag object signed with GPG
-
-collaborate (see also: git help workflows)
-   fetch     Download objects and refs from another repository
-   pull      Fetch from and integrate with another repository or a local branch
-   push      Update remote refs along with associated objects
-
-'git help -a' and 'git help -g' list available subcommands and some
-concept guides. See 'git help <command>' or 'git help <concept>'
-to read about a specific subcommand or concept.
-See 'git help git' for an overview of the system.
+#Time Complexity (worst case) : O((N-1)^2 + c)
+#Space Complexity : O(N + c)
